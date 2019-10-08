50461167f9e5c956925fe7f667144768594d3f5b
psk <p91s@outlook.com>
2019-10-07 23:17:53 -0500

    ZUUL API GATEWAY

--- currency-conversion-service/src/main/java/com/psksoft/microservices/currencyconversionservice/CuurencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/psksoft/microservices/currencyconversionservice/CuurencyExchangeProxy.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.psksoft.microservices.currencyconversionservice.bean.CurrencyConverionBean;
 
-@FeignClient(name="currency-exchange-service")
+/*@FeignClient(name="currency-exchange-service")*/
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CuurencyExchangeProxy {
-
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	
+	/*@GetMapping("/currency-exchange/from/{from}/to/{to}")*/
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConverionBean retriveExchangeValue(@PathVariable("from") String from, 
 			@PathVariable("to") String to); 
 }
